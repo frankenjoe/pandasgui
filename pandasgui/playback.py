528d79618866bee6c96fe3641affbbf20d8f4649
Johannes Wagner <10383417+frankenjoe@users.noreply.github.com>
2019-10-02 13:12:42 +0200

    remove debug playback

--- pandasgui/playback.py
+++ pandasgui/playback.py
@@ -20,7 +20,6 @@ def play_file(path: str, *, start: float = 0, end: float = None):
     global _play_channel
     duration = end - start if end else None
     x, sr = af.read(path, offset=start, duration=duration, always_2d=True)
-    print(_play_channel)
     if _play_channel is not None:
         if _play_channel >= x.shape[0]:
             warnings.warn('Cannot play channel ', _play_channel)
