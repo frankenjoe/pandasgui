e88da739c50c1ed5ad5758a9b58c0dafc3cc10ec
Johannes Wagner <10383417+frankenjoe@users.noreply.github.com>
2019-10-02 12:34:31 +0200

    select audio channel

--- pandasgui/playback.py
+++ pandasgui/playback.py
@@ -1,6 +1,15 @@
 import sounddevice as sd
 import numpy as np
 import audiofile as af
+import warnings
+
+
+_play_channel = None
+
+
+def set_channel(channel: int):
+    global _play_channel
+    _play_channel = channel
 
 
 def play(x: np.ndarray, sampling_rate: int):
@@ -8,7 +17,13 @@ def play(x: np.ndarray, sampling_rate: int):
 
 
 def play_file(path: str, *, start: float = 0, end: float = None):
+    global _play_channel
     duration = end - start if end else None
-    x, sr = af.read(path, offset=start, duration=duration)
+    x, sr = af.read(path, offset=start, duration=duration, always_2d=True)
+    if _play_channel is not None:
+        if _play_channel >= x.shape[0]:
+            warnings.warn('Cannot play channel ', _play_channel)
+            return
+        x = x[_play_channel, :]
     play(x, sr)
 
